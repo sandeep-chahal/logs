80f9ef39ef09fd2c66650c0d0a4ef3d3041c9a98
sandeep chahal <Sandeepchahal573@gmail.com>
2020-10-25 12:40:36 +0530

    added api route to delete comment

--- pages/api/post/deleteComment.js
+++ pages/api/post/deleteComment.js
@@ -7,32 +7,27 @@ import Post from "../../../models/post";
 import Comment from "../../../models/comment";
 
 export default withMiddlewares(
-	[withPassport, authorized, withValidation("post-comment")],
+	[withPassport, authorized, withValidation("post-id")],
 	async (req, res) => {
 		await dbConnect();
 
 		// check if post exist
-		const isPostExist = await Post.exists({ _id: req.body._id });
-		if (!isPostExist) {
+		const comment = await Comment.findById(req.body._id);
+		if (!comment) {
 			return res.json({
 				error: true,
 				code: 4, //404
-				msg: "This post doesn't exist anymore.",
+				msg: "This comment doesn't exist anymore.",
 			});
 		}
-		const comment = await Comment.create({
-			on_post: req.body._id,
-			by_user: req.user._id,
-			content: req.body.content,
-		});
-		await Post.findByIdAndUpdate(req.body._id, {
+		await Comment.findByIdAndDelete(req.body._id);
+		await Post.findByIdAndUpdate(comment.on_post, {
 			$inc: {
-				comments_counter: 1,
+				comments_counter: -1,
 			},
 		});
 		return res.json({
 			error: false,
-			data: comment,
 		});
 	}
 );
