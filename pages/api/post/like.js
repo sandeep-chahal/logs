bfd0c5907a8bdeffff66985ef91e7e8fc13a3169
sandeep chahal <Sandeepchahal573@gmail.com>
2020-10-25 11:41:00 +0530

    added api route to like and unlike post

---
+++ pages/api/post/like.js
@@ -0,0 +1,41 @@
+import withPassport from "../../../middlewares/withPassport";
+import withMiddlewares from "../../../middlewares/withMiddlewares";
+import withValidation from "../../../middlewares/withValidation";
+import authorized from "../../../middlewares/authorized";
+import dbConnect from "../../../config/mongodb";
+import Post from "../../../models/post";
+import Like from "../../../models/like";
+
+export default withMiddlewares([withPassport, authorized], async (req, res) => {
+	await dbConnect();
+
+	// check if post exist
+	const isPostExist = await Post.exists({ _id: req.body._id });
+	if (!isPostExist) {
+		return res.json({
+			error: true,
+			code: 4, //404
+			msg: "This post doesn't exist anymore.",
+		});
+	}
+	// check if the user already like the post
+	const alreadyLiked = await Like.exists({
+		on_post: req.body._id,
+		by_user: req.user._id,
+	});
+	if (!alreadyLiked) {
+		await Like.create({
+			on_post: req.body._id,
+			by_user: req.user._id,
+		});
+		await Post.findByIdAndUpdate(req.body._id, {
+			$inc: {
+				likes_counter: 1,
+			},
+		});
+	}
+
+	return res.json({
+		error: false,
+	});
+});
---
+++ pages/api/post/unlike.js
@@ -0,0 +1,32 @@
+import withPassport from "../../../middlewares/withPassport";
+import withMiddlewares from "../../../middlewares/withMiddlewares";
+import withValidation from "../../../middlewares/withValidation";
+import authorized from "../../../middlewares/authorized";
+import dbConnect from "../../../config/mongodb";
+import Post from "../../../models/post";
+import Like from "../../../models/like";
+
+export default withMiddlewares([withPassport, authorized], async (req, res) => {
+	await dbConnect();
+
+	// check if the user has liked the post
+	const hasLiked = await Like.exists({
+		on_post: req.body._id,
+		by_user: req.user._id,
+	});
+	if (hasLiked) {
+		await Like.findOneAndDelete({
+			on_post: req.body._id,
+			by_user: req.user._id,
+		});
+		await Post.findByIdAndUpdate(req.body._id, {
+			$inc: {
+				likes_counter: -1,
+			},
+		});
+	}
+
+	return res.json({
+		error: false,
+	});
+});
