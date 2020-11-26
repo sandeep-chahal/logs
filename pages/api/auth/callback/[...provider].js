d469ac1b6cd4171d3d308a198fdc0f8a22fc427f
sandeep chahal <Sandeepchahal573@gmail.com>
2020-11-26 14:09:11 +0530

    logged error from provider cb2

--- pages/api/auth/callback/[...provider].js
+++ pages/api/auth/callback/[...provider].js
@@ -10,6 +10,8 @@ export default async (req, res) => {
 
 	passport.authenticate(req.query.provider)(req, res, (err) => {
 		if (err) {
+			console.log(err);
+
 			if (err.code === "bad_verification_code")
 				return res.redirect("/?error_code=invalid_auth_code");
 			return res.redirect("/?error_code=unknown");
