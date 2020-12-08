6fe7d1a5a9fecbe620b40a6d983804152ce763e2
sandeep chahal <Sandeepchahal573@gmail.com>
2020-12-08 16:59:28 +0530

    added tag filtering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,52 @@
 import { GetServerSideProps } from "next";
-
+import React, { useState } from "react";
 import { getLatest } from "../services/redis";
 import Post from "../components/post";
-import Tags from "../components/tags";
 import { IShortPost } from "../models/post";
+
+const tags = ["javascript", "web", "tech", "nextjs", "nodejs", "reactjs"];
 
 interface IProps {
 	posts: null | IShortPost[];
 }
 
 const Home = ({ posts }: IProps) => {
+	const [selectedTag, setSelectedTag] = useState<string | null>(null);
+
+	const handleTagChange = (tag: string) => {
+		setSelectedTag((prev) => {
+			if (prev === tag) return null;
+			return tag;
+		});
+	};
 	return (
 		<div className="w-4/5 m-auto flex mt-5 items-baseline">
 			<div className="w-1/5 relative">
-				<Tags />
+				<div className="bg-white p-4 text-black fixed w-64">
+					<h3 className="text-2xl font-bold mb-3">Tags</h3>
+					<ul>
+						{tags.map((tag) => (
+							<li
+								onClick={() => handleTagChange(tag)}
+								key={tag}
+								className={`mb-2 cursor-pointer text-lg hover:text-primary ${
+									tag === selectedTag && "text-primary"
+								}`}
+							>
+								#{tag}
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 			<div className="w-2/3 m-auto">
 				<h1 className="text-3xl bg-white p-2 font-bold text-black ">
 					Latest Posts
 				</h1>
 				{Array.isArray(posts) && posts.length > 0 ? (
-					posts.map((post) => <Post key={post._id} post={post} />)
+					posts
+						.filter((post) => !selectedTag || post.tags.includes(selectedTag))
+						.map((post) => <Post key={post._id} post={post} />)
 				) : (
 					<div className="bg-white p-2 mt-3">No Posts Available!</div>
 				)}
