53cb5a9f0c3faabf3e65a2cadcd323d208f8ee48
sandeep chahal <Sandeepchahal573@gmail.com>
2020-12-12 14:46:14 +0530

    minor change in wndow size hook

--- utils/hooks/useWindowSize.tsx
+++ utils/hooks/useWindowSize.tsx
@@ -2,8 +2,8 @@ import React from "react";
 export default function useWindowSize() {
 	const isSSR = typeof window === "undefined";
 	const [windowSize, setWindowSize] = React.useState({
-		width: isSSR ? 1200 : window.innerWidth,
-		height: isSSR ? 800 : window.innerHeight,
+		width: isSSR ? 768 : window.innerWidth,
+		height: isSSR ? 768 : window.innerHeight,
 	});
 
 	function changeWindowSize() {
