0e84f5d88c300f5fe47ae55f7a4c821000570f1a
sandeep chahal <Sandeepchahal573@gmail.com>
2021-04-11 12:52:48 +0530

    stored db instance in global

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-const connection = {}; /* creating connection object*/
-
 async function dbConnect() {
+	console.log(
+		"-------------------------------global.mongo-------------------------------"
+	);
+	console.log(global.mongo);
 	/* check if we have connection to our database*/
-	if (connection.isConnected) {
+	if (global.mongo && global.mongo.isConnected) {
 		return;
 	}
-
+	console.log("creating new connection");
+	global.mongo = { isConnected: false };
 	/* connecting to our database */
 	const db = await mongoose.connect(process.env.MONGODB_URI, {
 		useNewUrlParser: true,
@@ -15,7 +18,7 @@ async function dbConnect() {
 		useFindAndModify: false,
 	});
 
-	connection.isConnected = db.connections[0].readyState;
+	global.mongo.isConnected = db.connections[0].readyState;
 }
 
 export default dbConnect;
