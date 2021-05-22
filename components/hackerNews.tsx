65093a2341af60c8a3ea42b7f3f4f05dc507ae4f
sandeepchahal573 <sandeepchahal573@gmail.com>
2021-05-22 08:23:37 +0530

    made hackerNews sticky

--- components/hackerNews.tsx
+++ components/hackerNews.tsx
@@ -4,7 +4,10 @@ import Link from "next/link";
 
 const HackerNews = (props: { news: IHackerNews[] }) => {
 	return (
-		<div className="relative bg-white md:p-4 w-11/12 md:w-1/3 md:mr-12">
+		<div
+			style={{ top: "80px" }}
+			className="hacker-news md:sticky bg-white md:p-4 w-11/12 md:w-1/3 md:mr-12 overflow-y-auto"
+		>
 			<h2 className="text-xl font-bold mb-4">Latest Hacker News</h2>
 			<div>
 				{props.news.map((news) => (
